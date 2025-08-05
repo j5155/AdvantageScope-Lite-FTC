e0039952e442d832841ca66cc1e120519ba727c1
j5155 <54331556+j5155@users.noreply.github.com>
2025-08-04 17:28:58 -0800

    Asset fixes

--- src/main/kotlin/page/j5155/rrscopelite/RRScopeLite.kt
+++ src/main/kotlin/page/j5155/rrscopelite/RRScopeLite.kt
@@ -1,6 +1,5 @@
 package page.j5155.rrscopelite
 
-import android.R.attr.path
 import android.content.Context
 import android.content.res.AssetManager
 import com.google.gson.JsonObject
@@ -25,6 +24,10 @@ object RRScopeLite {
     const val WEB_ROOT = "/as"
     const val EXTRA_ASSETS_PATH = "ascope_assets/"
     val EXTRA_ASSETS = File(AppUtil.ROOT_FOLDER, EXTRA_ASSETS_PATH)
+
+    init {
+        if (!EXTRA_ASSETS.exists()) EXTRA_ASSETS.mkdir()
+    }
     const val BUNDLED_ASSETS_PATH = "as/bundledAssets/"
     val ALLOWED_LOG_SUFFIXES = arrayOf(".wpilog", ".rlog", ".log")
 
@@ -55,12 +58,20 @@ object RRScopeLite {
         }
     }
 
-    val extraAssets = JsonObject()
+    var extraAssets = JsonObject()
 
     init {
+        readExtraAssets()
+    }
+
+    /**
+     * Returns: whether to update web handlers
+     * todo: does this impl make sense?
+     */
+    private fun readExtraAssets(): Boolean {
         // read and cache config.json for all extra assets
-        // ideally we would handle adding extra assets without an app restart, but this will work for now
         val extraAssetFiles = EXTRA_ASSETS.listFiles()
+        val newExtraAssets = JsonObject()
         extraAssetFiles?.forEach { assetFile ->
             val containedFiles = assetFile.listFiles()
             containedFiles?.forEach { file ->
@@ -68,12 +79,19 @@ object RRScopeLite {
                 if (file.name == "config.json") {
                     val fileReader = file.reader()
 
-                    extraAssets.add(path, jsonParser.parse(fileReader))
+                    newExtraAssets.add(path, jsonParser.parse(fileReader))
                     fileReader.close()
                 } else {
-                    extraAssets.add(path,null)
+                    newExtraAssets.add(path,null)
                 }
             }
+        }
+
+        if (newExtraAssets != extraAssets) {
+            extraAssets = newExtraAssets
+            return true
+        } else {
+            return false
         }
     }
 
@@ -140,6 +158,15 @@ object RRScopeLite {
         return object : WebHandler {
             override fun getResponse(session: IHTTPSession): NanoHTTPD.Response {
                 if (session.method == NanoHTTPD.Method.GET) {
+                    if (readExtraAssets()) { // check the extra assets folder, and if there are more assets register them
+                        extraAssets.entrySet().forEach { (path, _) ->
+                            webHandlerManager.register(
+                                "$WEB_ROOT/assets/$path",
+                                newStaticFileHandler(EXTRA_ASSETS_PATH + path)
+                            )
+                        }
+                    }
+
                     // order matters;
                     // if an extra asset is added with the same name as a bundled asset,
                     // it will override the bundled one
